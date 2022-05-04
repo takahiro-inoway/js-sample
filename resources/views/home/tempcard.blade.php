d56d2608582bdbcb5acf448e9f43514c37e9811e
takahiro-inoway <takahiro.inoway@gmail.com>
2022-05-04 16:01:26 +0900

    [add]220504_b

--- resources/views/home/tempcard.blade.php
+++ resources/views/home/tempcard.blade.php
@@ -1,6 +1,33 @@
-
 <section>
+  <h2>応対支援システム</h2>
   <div class="tempcard_outline">
-    <div class="tempcard">card</div>
+    @for($i = 0; $i < 3; $i++) <div class="tempcard">
+      <div class="tempcard_head">head</div>
+      <textarea placeholder="textarea"></textarea>
+      <div class="tempcard_foot">foot</div>
+    </div>
+    @endfor
   </div>
+
+  <div id="tempcard_btn_outline">
+    <div id="tempcard_item_btns" class="btn_block">
+      <input type="button" value="りんご">
+      <input type="button" value="みかん">
+      <input type="button" value="バナナ">
+      <input type="button" value="ぶどう">
+      <input type="button" value="オレンジ">
+    </div>
+    <div id="tempcard_storage_btns" class="btn_block">
+      <input type="button" value="東京倉庫">
+      <input type="button" value="大阪倉庫">
+      <input type="button" value="那覇倉庫">
+    </div>
+    <div id="tempcard_pay_btns" class="btn_block">
+      <input type="button" value="後払決済">
+      <input type="button" value="クレジット">
+      <input type="button" value="代引">
+      <input type="button" value="講座振替">
+    </div>
+  </div>
+
 </section>
